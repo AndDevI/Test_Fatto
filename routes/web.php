ff444bf44b2975d5a9a5f1ce06fa8276e3d60779
Andrew Raphael <andrewrgpires@gmail.com>
2024-11-14 10:12:50 -0300

    feat: Define rotas para operações CRUD de Tarefa

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\TarefaController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/tarefas', [TarefaController::class, 'index'])->name('tarefas.index');
+Route::post('/tarefas', [TarefaController::class, 'store'])->name('tarefas.store');
+Route::put('/tarefas/{tarefa}', [TarefaController::class, 'update'])->name('tarefas.update');
+Route::put('/tarefas/{tarefa}', [TarefaController::class, 'destroy'])->name('tarefas.destroy');
