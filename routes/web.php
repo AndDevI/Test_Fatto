16495abeba19a99293c0237e5852f2d0c61e5b9f
Andrew Raphael <andrewrgpires@gmail.com>
2024-11-14 10:20:24 -0300

    fix: Resolve erro de sintaxe nas rotas do CRUD de Taref

--- routes/web.php
+++ routes/web.php
@@ -6,4 +6,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/tarefas', [TarefaController::class, 'index'])->name('tarefas.index');
 Route::post('/tarefas', [TarefaController::class, 'store'])->name('tarefas.store');
 Route::put('/tarefas/{tarefa}', [TarefaController::class, 'update'])->name('tarefas.update');
-Route::put('/tarefas/{tarefa}', [TarefaController::class, 'destroy'])->name('tarefas.destroy');
+Route::delete('/tarefas/{tarefa}', [TarefaController::class, 'destroy'])->name('tarefas.destroy');
