0aa7be886df3fee92dd80fb8317610c3d412c3e2
Andrew Raphael <andrewrgpires@gmail.com>
2024-11-18 10:01:50 -0300

    refactor: Atualiza nome da variável no modal de edição

--- public/js/EditModal.js
+++ public/js/EditModal.js
@@ -1,4 +1,4 @@
-const modal = document.getElementById('edit_modal');
+const editModal = document.getElementById('edit_modal');
 
 function openEditModal(id, nome, custo, dataLimite) {
     document.getElementById('edit-nome').value = nome;
@@ -7,15 +7,15 @@ function openEditModal(id, nome, custo, dataLimite) {
 
     document.getElementById('edit-form').action = `/tarefas/${id}`;
            
-    modal.classList.remove('hidden');
+    editModal.classList.remove('hidden');
     setTimeout(() => {
-        modal.classList.remove('opacity-0'); 
+        editModal.classList.remove('opacity-0'); 
     }, 10); 
 }
 
 function closeEditModal() {
-    modal.classList.add('opacity-0'); 
+    editModal.classList.add('opacity-0'); 
     setTimeout(() => {
-        modal.classList.add('hidden'); 
+        editModal.classList.add('hidden'); 
     }, 300); 
 }
