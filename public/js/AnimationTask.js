53c35ac35c61c058d4c95331a1b4d35e5999961d
Andrew Raphael <andrewrgpires@gmail.com>
2024-11-19 22:01:04 -0300

    feat: Implementa scroll suave ao criar nova tarefa

--- public/js/AnimationTask.js
+++ public/js/AnimationTask.js
@@ -1,6 +1,11 @@
 document.addEventListener('DOMContentLoaded', () => {
     const novaTarefa = document.querySelector('.animate-highlight');
     if (novaTarefa) {
+        novaTarefa.scrollIntoView({
+            behavior: 'smooth', 
+            block: 'center',    
+        });
+        
         setTimeout(() => {
             novaTarefa.classList.remove('animate-highlight');
         }, 2000); 
